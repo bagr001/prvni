57c44896cb1ec5efb6b18d98b12e4b8f198b1070
bagr001 <vojta@lankovi.cz>
2015-09-26 11:15:58 +0300

    fully working

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
-include './Classes/MyClass.php';
-include './Classes/TestClass.php.php';
+define('CLASS_DIR', __DIR__ . '/Classes');
+define('INTERFACE_DIR', __DIR__ . '/Interfaces');
+
+include INTERFACE_DIR . '/IMath.php';
+include CLASS_DIR . '/MyClass.php';
+include CLASS_DIR . '/TestClass.php';
 
 echo '[1]: ' . MyClass::foo();
 echo '[2]: ' . MyClass::foo();
