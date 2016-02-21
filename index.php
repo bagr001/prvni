0c3b1a2da01e058b6384fc4916fb261d0f8ffa24
bagr001 <vojta@lankovi.cz>
2016-02-21 11:25:48 +0100

    TestClass::bar v indexu

--- index.php
+++ index.php
@@ -20,3 +20,5 @@ echo '[n*n]: ' . TestClass::multiply(Counter::inc()) . EOL . EOL;
 echo '[sqrt(n)]: ' . TestClass::sqrt(Counter::inc()) . EOL . EOL;
 
 echo '[count 3]: ' . Counter::count() . EOL . EOL;
+
+TestClass::bar('ahoj');
